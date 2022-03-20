b85baf1824138809946efe08bcb4959ccae1b698
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-03-20 21:14:51 +0800

    Create settings.py

---
+++ app/settings.py
@@ -0,0 +1,13 @@
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APP = [
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'django.contrib.staticfiles',
+  'store.app.StoreConfig',
+]
