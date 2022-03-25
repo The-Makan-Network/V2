088e09964da900e72eea18f794d41db8ee9e7c71
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-03-26 01:56:02 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,13 +1,140 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import connection
 
-def store(request):
-     context = {}
-     return render(request, 'store/store.html', context)
+# Create your views here.
+def index(request):
+    """Shows the main page"""
 
-def cart(request):
-     context = {}
-     return render(request, 'store/cart.html', context)
+    ## Delete customer
+    if request.POST:
+        if request.POST['action'] == 'delete':
+            with connection.cursor() as cursor:
+                cursor.execute("DELETE FROM customers WHERE customerid = %s", [request.POST['id']])
 
-def checkout(request):
-      context = {}
-      return render(request, 'store/checkout.html', context)
+    ## Use raw query to get all objects
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM customers ORDER BY customerid")
+        customers = cursor.fetchall()
+
+    result_dict = {'records': customers}
+
+    return render(request, 'app/index_user_admin.html', result_dict)
+
+# Create your views here.
+def view(request, id):
+    """Shows the main page"""
+    
+    ## Use raw query to get a customer
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM customers WHERE customerid = %s", [id])
+        customer = cursor.fetchone()
+    result_dict = {'cust': customer}
+
+    return render(request, 'app/view_admin.html', result_dict)
+
+# Create your views here.
+def add(request):
+    """Shows the main page"""
+    context = {}
+    status = ''
+
+    if request.POST:
+        ## Check if customerid is already in the table
+        with connection.cursor() as cursor:
+
+            cursor.execute("SELECT * FROM customers WHERE customerid = %s", [request.POST['customerid']])
+            customer = cursor.fetchone()
+            ## No customer with same id
+            if customer == None:
+                ##TODO: date validation
+                cursor.execute("INSERT INTO customers VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                        , [request.POST['first_name'], request.POST['last_name'], request.POST['email'],
+                           request.POST['dob'] , request.POST['since'], request.POST['customerid'], request.POST['country'] ])
+                return redirect('index')    
+            else:
+                status = 'Customer with ID %s already exists' % (request.POST['customerid'])
+
+
+    context['status'] = status
+ 
+    return render(request, "app/new_user.html", context)
+
+# Create your views here.
+def edit(request, id):
+    """Shows the main page"""
+
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+
+    # fetch the object related to passed id
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM customers WHERE customerid = %s", [id])
+        obj = cursor.fetchone()
+
+    status = ''
+    # save the data from the form
+
+    if request.POST:
+        ##TODO: date validation
+        with connection.cursor() as cursor:
+            cursor.execute("UPDATE customers SET first_name = %s, last_name = %s, email = %s, dob = %s, since = %s, country = %s WHERE customerid = %s"
+                    , [request.POST['first_name'], request.POST['last_name'], request.POST['email'],
+                        request.POST['dob'] , request.POST['since'], request.POST['country'], id ])
+            status = 'Customer edited successfully!'
+            cursor.execute("SELECT * FROM customers WHERE customerid = %s", [id])
+            obj = cursor.fetchone()
+
+
+    context["obj"] = obj
+    context["status"] = status
+ 
+    return render(request, "app/edit_admin.html", context)
+
+# Create your views here.
+def login(request):
+    """Shows the login page"""
+    context = {}
+    status = ''
+
+    if request.POST:
+        ## Check if customerid is already in the table
+        with connection.cursor() as cursor:
+
+            cursor.execute("SELECT * FROM customers WHERE customerid = %s", [request.POST['customerid']])
+            customer = cursor.fetchone()
+            ## No customer with same id
+            if customer == None:
+                cursor.execute("INSERT INTO customers VALUES (%s, %s)"
+                        , [request.POST['first_name'], request.POST['last_name']])
+                return redirect('index')
+            else:
+                status = 'Your User Id and Password is incorrect' % (request.POST['customerid'])
+
+    return render(request, 'app/login.html', context)
+
+
+# Create your views here.
+def index_products(request):
+    """Shows the product listing page"""
+
+    ## Use raw query to get all objects
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM products ORDER BY productid")
+        products = cursor.fetchall()
+
+    result_dict = {'products': products}
+
+    return render(request, 'app/index_products.html', result_dict)
+
+def purchase(request, productid):
+    """Shows the main page"""
+
+    ## Use raw query to get all objects
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM products WHERE productid =  %s",[productid])
+        products = cursor.fetchone()
+
+    result_dict = {'products': products}
+
+    return render(request, 'app/purchase.html', result_dict)
