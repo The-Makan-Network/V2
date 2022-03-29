30c88e2ee5e669ea4be37246a37c27eced5e997f
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-03-30 00:59:18 +0800

    Create forms.py

---
+++ app/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+# Create your forms here.
+
+class NewUserForm(UserCreationForm):
+	number = forms.IntegerField(required=True)
+
+	class Meta:
+		model = User
+		fields = ("userid", "phoneno", "password")
+
+	def save(self, commit=True):
+		user = super(NewUserForm, self).save(commit=False)
+		user.number = self.cleaned_data['number']
+		if commit:
+			user.save()
+		return user
