aa465c1971aa51b556de3288dbe8f5fd56153381
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-03-30 01:18:15 +0800

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -7,17 +7,17 @@ class UserAdmin(admin.ModelAdmin):
   list_display = ('userid', 'phoneno', 'password')
   
   fields = ['userid', 'phoneno', 'password']
-    pass
+
 
 admin.site.register(User, AuthorAdmin)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
   list_display = ('productid', 'name', 'description', 'minorder')
-    pass
+
 
   
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
   list_filter = ('status')
-    pass
+
