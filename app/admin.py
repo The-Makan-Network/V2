e4afe4301b35fc3d475776977e4052f32060542c
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-03-30 01:20:05 +0800

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -2,22 +2,3 @@ from django.contrib import admin
 
 # Register your models here.
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-  list_display = ('userid', 'phoneno', 'password')
-  
-  fields = ['userid', 'phoneno', 'password']
-
-
-admin.site.register(User, AuthorAdmin)
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-  list_display = ('productid', 'name', 'description', 'minorder')
-
-
-  
-@admin.register(Transaction)
-class TransactionAdmin(admin.ModelAdmin):
-  list_filter = ('status')
-
