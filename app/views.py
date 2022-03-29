c955e26aa3917ce0a9ba2f1e5aa3325093dd7832
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-03-30 04:24:10 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -57,18 +57,18 @@ def signin(request):
 
     if request.POST:
         form = AuthenticationForm(request, data=request.POST)
-	if form.is_valid():
-		username = form.cleaned_data.get('username')
-		password = form.cleaned_data.get('password1')
-		user = authenticate(username=username, password1=password1)
-		if user is not None:
-			login(request, user)
-			messages.info(request, f"You are now logged in as {username}.")
-			return redirect("/")
-		else:
-			messages.error(request,"Invalid username or password.")
-	else:
-		status = 'Invalid username or password.' 
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password1=password1)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("/")
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            status = 'Invalid username or password.' 
     form = AuthenticationForm()
     context['status'] = status
     return render(request, 'app/login.html', context)
