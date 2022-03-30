14ecb7ed452ce966a43cb3edd0695442e3f3286c
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-03-30 16:58:13 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -58,8 +58,7 @@ def signin(request):
     if request.POST:
         username = request.POST['username']
         password = request.POST['password1']
-        
-	authenticate(username=username, password1=password1)
+        authenticate(username=username, password1=password1)
 	
 	if user is not None:
             login(request, user)
