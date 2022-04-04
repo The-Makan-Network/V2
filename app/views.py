59f97987c4e1b2e038d204167813f5bbae32d0a3
Aeromon <e0421426@u.nus.edu>
2022-04-04 22:11:59 +0800

    Views.py updated with Admin_users and Admin_edit Html files updated

--- app/views.py
+++ app/views.py
@@ -18,8 +18,6 @@ def home(request):
 
     return render(request, 'app/home.html', result_dict)
 
-
-
 # Create your views here.
 def register(request):
 
@@ -65,12 +63,13 @@ def signin(request):
     else:
         return render(request, 'app/login.html', {})
 
+
 def signout(request):
 	logout(request)
 	messages.success(request, ("You Were Logged Out!"))
 	return redirect('home')
-			
-		
+
+
 def profile(request, id):
     """Shows the main page"""
 
@@ -94,3 +93,50 @@ def view(request, id):
     result_dict = {'cust': customer}
 
     return render(request,'app/view.html',result_dict)
+
+def admin_users(request):
+    """Show list of allusers with buttons to edit/delete"""
+
+    ## Delete customer
+    if request.POST:
+        if request.POST['action'] == 'delete':
+            with connection.cursor() as cursor:
+                cursor.execute("DELETE FROM allusers WHERE phoneno = %s", [request.POST['id']])
+
+    ## Use raw query to get all objects
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM allusers ORDER BY phoneno")
+        users = cursor.fetchall()
+
+    result_dict = {'records': users}
+
+    return render(request,'app/admin_users.html',result_dict)
+
+def admin_edit(request, id):
+    """Shows the admin_users_edit page"""
+
+    # dictionary for initial data with
+    # field names as keys
+    context = {}
+
+    # fetch the object related to passed id
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM allusers WHERE phoneno = %s", [id])
+        obj = cursor.fetchone()
+
+    status = ''
+    # save the data from the form
+
+    if request.POST:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "UPDATE allusers SET user_id = %s, password = %s, email = %s, country = %s WHERE phoneno = %s"
+                , [request.POST['user id'], request.POST['password'], request.POST['phoneno'], id])
+            status = 'Customer edited successfully!'
+            cursor.execute("SELECT * FROM allusers WHERE phoneno = %s", [id])
+            obj = cursor.fetchone()
+
+    context["obj"] = obj
+    context["status"] = status
+
+    return render(request, "app/admin_users_edit.html", context)
