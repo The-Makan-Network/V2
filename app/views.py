0598ae46e7a6c6e9173bab87a4e99fb304023941
kohlongyang <77948776+kohlongyang@users.noreply.github.com>
2022-04-06 14:48:47 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -50,8 +50,7 @@ def register(request):
 def sell(request):
     if request.POST:
 	with connection.cursor() as cursor:
-	    cursor.execute("INSERT INTO products(productid, sellerid, name, description, price, category, allergen, minorder) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-		    , [request.POST['productid'], request.POST['username'], request.POST['foodname'], request.POST['description'], request.POST['price'], request.POST['category'], request.POST['allergen'], request.POST['minorder'] ])
+	    cursor.execute("INSERT INTO products(productid, sellerid, name, description, price, category, allergen, minorder) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", [request.POST['productid'], request.POST['username'], request.POST['foodname'], request.POST['description'], request.POST['price'], request.POST['category'], request.POST['allergen'], request.POST['minorder'] ])
 	    return redirect('home')
 			        
 	
